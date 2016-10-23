431fdbd03b4ab36b4e57cb2cdbfdc4efe40f82a6
Dohyung Ahn <emaren84@gmail.com>
2016-10-23 18:30:43 +0900

    updating book through api server

--- apps/api/controllers/books/update.rb
+++ apps/api/controllers/books/update.rb
@@ -2,8 +2,32 @@ module Api::Controllers::Books
   class Update
     include Api::Action
 
-    def call(params)
-      self.body = 'OK'
+    params do
+      required(:id).filled(:int?)
+      required(:title).filled(:str?)
+      required(:author).filled(:str?)
     end
+
+    def call(params)
+      update = Book::Update.new(params[:id], params[:title], params[:author])
+      result = update.call
+
+      if result.success?
+        render_success(result.book)
+      else
+        render_failure
+      end
+    end
+    
+    private
+
+      def render_success(book)
+        self.status = 200
+        self.body = Api::Representers::Book.new(book).to_json
+      end
+
+      def render_failure
+        halt 404, JSON.generate(error_type: :not_found)
+      end
   end
 end
---
+++ lib/bookshelf_delivery_clone/interactors/book/update.rb
@@ -0,0 +1,40 @@
+require 'hanami/interactor'
+
+class Book::Update
+  include Hanami::Interactor
+
+  expose :activity, :book
+
+  def initialize(id, title, author)
+    @book = BookRepository.find(id)
+    @title = title
+    @author = author
+  end
+
+  def call
+    if @book
+      @book.update(title: @title, author: @author)
+      @activity = build_activity
+
+      BookRepository.transaction do
+        BookRepository.update(@book)
+        ActivityRepository.create(@activity)
+      end
+    else
+      fail!
+    end
+  end
+
+  private
+
+  def build_activity
+    Activity.new(
+        timestamp: Time.now,
+        action: Activity::Action::UPDATE,
+        old_title: @book.title,
+        old_author: @book.author,
+        new_title: @title,
+        new_author: @author
+    )
+  end
+end
