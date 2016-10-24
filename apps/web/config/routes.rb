c1bed215c47ebd004f8238c7987c5e334fc389a7
Dohyung Ahn <emaren84@gmail.com>
2016-10-24 10:01:29 +0900

    Initialize web app & listing books

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,5 +1,10 @@
-# Configure your routes here
-# See: http://hanamirb.org/guides/routing/overview/
-#
-# Example:
-# get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+delete '/books/:id', to: 'books#destroy'
+patch '/books/:id', to: 'books#update'
+get '/books/:id/edit', to: 'books#edit'
+get '/books/new', to: 'books#new'
+post '/books', to: 'books#create'
+get '/books', to: 'books#index'
+get '/', to: 'home#index', as: :home
+
+resources :activities, onlly: [:index]
+resources :books
---
+++ apps/web/controllers/books/destroy.rb
@@ -0,0 +1,8 @@
+module Web::Controllers::Books
+  class Destroy
+    include Web::Action
+
+    def call(params)
+    end
+  end
+end
