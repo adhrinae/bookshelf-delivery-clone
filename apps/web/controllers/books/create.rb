0b613491e50c33387f607de10120665d034ed0f2
Dohyung Ahn <emaren84@gmail.com>
2016-10-24 10:44:15 +0900

    create books in the browser

--- apps/web/controllers/books/create.rb
+++ apps/web/controllers/books/create.rb
@@ -2,7 +2,23 @@ module Web::Controllers::Books
   class Create
     include Web::Action
 
+    expose :book
+
+    params do
+      required(:book).schema do
+        required(:title).filled(:str?)
+        required(:author).filled(:str?)
+      end
+    end
+
     def call(params)
+      return unless params.valid?
+
+      title, author = params[:book].to_h.values_at(:title, :author)
+      result = Book::Create.new(title, author).call
+      @book = result.book
+
+      redirect_to routes.books_path
     end
   end
 end
--- apps/web/views/books/create.rb
+++ apps/web/views/books/create.rb
@@ -1,5 +1,15 @@
 module Web::Views::Books
   class Create
     include Web::View
+
+    template 'books/new'
+
+    def form
+      Form.new(:book, routes.books_path)
+    end
+
+    def submit_label
+      'Create Book'
+    end
   end
 end
