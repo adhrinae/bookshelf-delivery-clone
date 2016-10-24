7fc4a2b074bd3e6e1f23d8ffd5c5031927e11378
Dohyung Ahn <emaren84@gmail.com>
2016-10-24 10:55:13 +0900

    editing book in the browser

--- apps/web/controllers/books/update.rb
+++ apps/web/controllers/books/update.rb
@@ -2,7 +2,26 @@ module Web::Controllers::Books
   class Update
     include Web::Action
 
+    expose :book
+
+    params do
+      required(:id).filled(:int?)
+      required(:book).schema do
+        required(:title).filled(:str?)
+        required(:author).filled(:str?)
+      end
+    end
+
     def call(params)
+      if params.valid?
+        title, author = params[:book].to_h.values_at(:title, :author)
+        result = Book::Update.new(params[:id], title, author).call
+        @book = result.book
+
+        redirect_to routes.books_path
+      else
+        @book = BookRepository.find(params[:id])
+      end
     end
   end
 end
