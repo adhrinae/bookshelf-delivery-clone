4943a17df8fc5bbce62facfcc45d15184a944198
Dohyung Ahn <emaren84@gmail.com>
2016-10-24 11:22:04 +0900

    deleting book in the browser

--- apps/web/controllers/books/destroy.rb
+++ apps/web/controllers/books/destroy.rb
@@ -2,7 +2,13 @@ module Web::Controllers::Books
   class Destroy
     include Web::Action
 
+    params do
+      required(:id).filled(:int?)
+    end
+
     def call(params)
+      Book::Destroy.new(params[:id]).call
+      redirect_to routes.books_path
     end
   end
 end
--- spec/web/controllers/books/destroy_spec.rb
+++ spec/web/controllers/books/destroy_spec.rb
@@ -2,11 +2,16 @@ require 'spec_helper'
 require_relative '../../../../apps/web/controllers/books/destroy'
 
 describe Web::Controllers::Books::Destroy do
+  let(:book) { Book::Create.new('A title', 'An author').call.book }
   let(:action) { Web::Controllers::Books::Destroy.new }
-  let(:params) { Hash[] }
+  let(:params) { Hash[id: book.id] }
+
+  before do
+    BookRepository.clear
+  end
 
   it 'is successful' do
     response = action.call(params)
-    response[0].must_equal 200
+    response[0].must_equal 302
   end
 end
